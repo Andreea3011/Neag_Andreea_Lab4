391394230718e99367143f43179308b213bb9058
Andreea Neag <neag_andreea@yahoo.com>
2021-10-30 12:40:10 +0300

    public

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,9 @@ using System.Windows.Shapes;
 
 namespace Neag_Andreea_Lab2
 {
-    
+    /// <summary>
     /// Interaction logic for MainWindow.xaml
-  ]
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
